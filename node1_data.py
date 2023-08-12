fe8c6782864c78e32edeaa7f1f7ba98c17ed6747
Maitham Al-rubaye <126909774+Maitham16@users.noreply.github.com>
2023-08-12 22:45:43 +0200

    practical1__data steam process

---
+++ node1_data.py
@@ -0,0 +1,28 @@
+import csv
+from kafka import KafkaConsumer
+import json
+
+def start_consumer(topic_name):
+    consumer = KafkaConsumer(
+        topic_name,
+        bootstrap_servers='localhost:9092',
+        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+    )
+
+    # Create a CSV file and write the headers
+    with open(f'{topic_name}.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["car_id", "model", "current_speed", "battery_capacity", 
+                        "charge", "consumption", "engine_power", "engine_torque",
+                        "location", "node", "charging", "distance_covered"])
+
+        # Iterate over the messages in the Kafka stream
+        for msg in consumer:
+            data = msg.value
+            print(f"Received data from {topic_name}: {data}")
+            
+            # Write the data to the CSV file
+            writer.writerow([data[key] for key in data])
+
+if __name__ == "__main__":
+    start_consumer('node1_data')
