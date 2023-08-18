dfc75aaeb15c1369f78bc26012d5b31b1079d532
Maitham Al-rubaye <maythem.altamar@gmail.com>
2023-08-18 05:00:51 +0200

    update cars scripts and kafka nodes data receivers

--- car2.py
+++ car2.py
@@ -3,90 +3,121 @@ import json
 import time
 import random
 
-producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+# Constants
+BASE_SPEED = 90  # km/h at which the given consumption rate is measured
+CONSUMPTION_MODIFIER = 0.002  # arbitrary factor, adjust for realism
+CHARGING_STATIONS = [150, 270, 460]  # Charging station locations
+BATTERY_LIFE_DEGRADATION_RATE = 0.001
+DEGRADATION_FACTOR = 0.5
 
-# Initial data
-data = {
-    'car_id': 'ABE456',
-    'model': 'Model G',
-    'current_speed': 85,  # km/h
-    'battery_capacity': 50,  # kWh  
-    'charge': 99,  # %  (percentage)
-    'consumption': 0.20,  # kWh/1 km  
-    'engine_power': 90,  # kW
-    'engine_torque': 200,  # Nm
-    'location': 66,  # km
-    'node': 'Node 1',
-    'charging': False,
-    'distance_covered': 0,
-}
+def create_producer():
+    return KafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
 
-# Time step in minutes
-time_step = 0.1
+def update_car_data_while_charging(data):
+    data['charge'] += 30  # 30% charge per 30 minutes
+    if random.random() < 0.5:  # 50% chance to stop charging each time step
+        data['charging'] = False
 
-# Distance covered in each time step (in km)
-distance_step = data['current_speed'] * (time_step/60)
+def update_car_data_while_moving(data, distance_step):
+    data['location'] += distance_step
+    data['distance_covered'] += distance_step
+    speed_factor = data['current_speed'] / BASE_SPEED
+    adjusted_consumption = data['consumption'] * speed_factor + (CONSUMPTION_MODIFIER * (speed_factor - 1))
+    modified_consumption = adjusted_consumption * (1 + ((100 - data['battery_life']) / 100) * DEGRADATION_FACTOR)
+    data['charge'] -= modified_consumption * distance_step / data['battery_capacity'] * 100
+    data['battery_life'] -= BATTERY_LIFE_DEGRADATION_RATE
 
-charging_stations = [150, 270, 460]  # Charging station locations
-
-for i in range(int(600/distance_step)):
-    if data['charge'] <= 0:
-        break
-
-    # If the car is charging
-    if data['charging']:
-        # Increase charge
-        data['charge'] += 30  # 30% charge per 30 minutes
-
-        # Randomly decide to stop charging
-        if random.random() < 0.5:  # 50% chance to stop charging each time step
-            data['charging'] = False
-
-    # If the car is not charging
-    else:
-        # Update location
-        data['location'] += distance_step
-        data['distance_covered'] += distance_step
-
-        # Update node based on location
-        if data['location'] < 150:
-            data['node'] = 'Node 1'
-        elif data['location'] < 270:
-            data['node'] = 'Node 2'
-        elif data['location'] < 460:
-            data['node'] = 'Node 3'
-        else:
-            data['node'] = 'Node 4'
-
-        # Update charge based on consumption
-        data['charge'] -= data['consumption'] * distance_step / data['battery_capacity'] * 100
-
-        # If at a charging station and charge is low, start charging
-        if data['location'] in charging_stations and data['charge'] <= 30:  # Start charging if charge is 30% or less
+    if data['charge'] <= 30 and data['location'] not in CHARGING_STATIONS:
+        next_station = next((station for station in CHARGING_STATIONS if station > data['location']), None)
+        if next_station:
+            data['location'] = next_station
             data['charging'] = True
+    
+    speed_variation = 10
+    min_speed = 10
+    max_speed = 180
+    data['current_speed'] = random.randint(max(data['current_speed'] - speed_variation, min_speed), 
+                                        min(data['current_speed'] + speed_variation, max_speed))
 
-    # Print current status
-    print(f"Time step: {i*time_step} minutes")
-    print(f"Current speed: {data['current_speed']} km/h")
-    print(f"Distance covered in this step: {distance_step} km")
-    print(f"Charge consumed in this step: {data['consumption'] * distance_step} %")
-    print(f"Remaining charge: {data['charge']} %")
-    print(f"Total distance covered: {data['distance_covered']} km")
-    print("--------------------------")
-
-    # Send data to Kafka
-    producer.send('electric_car_data', data)
-    if data['node'] == 'Node 1':
-        producer.send('node1_data', data)
-    elif data['node'] == 'Node 2':
-        producer.send('node2_data', data)
-    elif data['node'] == 'Node 3':
-        producer.send('node3_data', data)
+    if data['location'] < 150:
+        data['node'] = 'Node 1'
+    elif data['location'] < 270:
+        data['node'] = 'Node 2'
+    elif data['location'] < 460:
+        data['node'] = 'Node 3'
     else:
-        producer.send('node4_data', data)
+        data['node'] = 'Node 4'
 
-    # Wait for the next time step
-    time.sleep(time_step * 60)  # convert to seconds
+    return modified_consumption
+
+def main():
+    producer = create_producer()
+
+    # Initial data
+    data = {
+        'car_id': 'ABE456',
+        'model': 'Model G',
+        'current_speed': 85,  # km/h
+        'battery_capacity': 50,  # kWh  
+        'charge': 99,  # %  (percentage)
+        'consumption': 0.20,  # kWh/1 km  
+        'engine_power': 90,  # kW
+        'engine_torque': 200,  # Nm
+        'location': 66,  # km
+        'node': 'Node 1',
+        'charging': False,
+        'distance_covered': 0,
+        'battery_life': 100  # %, 100 being a brand new battery
+    }
+
+    time_step = 1
+    distance_step = data['current_speed'] * (time_step/60)
+
+    counter = 0
+
+    while data['charge'] > 0 and data['distance_covered'] < 600:
+        counter += 1  # Increment counter with each loop iteration
+        
+        if data['charging']:
+            update_car_data_while_charging(data)
+        else:
+            modified_consumption = update_car_data_while_moving(data, distance_step)
+        
+        # Recalculate the distance_step based on current speed
+        distance_step = data['current_speed'] * (time_step/60)
+
+        # Ensure charge does not go negative
+        if data['charge'] < 0:
+            data['charge'] = 0
+            print("Car has run out of charge and has stopped.")
+            break
+        
+        # Print current status
+        print(f"Time step: {counter*time_step} minutes")
+        print(f"Current speed: {data['current_speed']} km/h")
+        print(f"Distance covered in this step: {distance_step} km")
+        print(f"Charge consumed in this step: {modified_consumption * distance_step} %")
+        print(f"Remaining charge: {data['charge']} %")
+        print(f"Total distance covered: {data['distance_covered']} km")
+        print(f"Battery life: {data['battery_life']} %")
+        print("--------------------------")
+
+        # Send data to Kafka
+        producer.send('electric_car_data', data)
+        if data['node'] == 'Node 1':
+            producer.send('node1_data', data)
+        elif data['node'] == 'Node 2':
+            producer.send('node2_data', data)
+        elif data['node'] == 'Node 3':
+            producer.send('node3_data', data)
+        else:
+            producer.send('node4_data', data)
+        
+        # Sleep before moving to the next step
+        time.sleep(time_step * 60)  # convert to seconds
+
+if __name__ == "__main__":
+    main()
--- node2_data.py
+++ node2_data.py
@@ -1,6 +1,7 @@
 import csv
 from kafka import KafkaConsumer
 import json
+from datetime import datetime
 
 def start_consumer(topic_name):
     consumer = KafkaConsumer(
@@ -12,17 +13,22 @@ def start_consumer(topic_name):
     # Create a CSV file and write the headers
     with open(f'{topic_name}.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["car_id", "model", "current_speed", "battery_capacity", 
-                        "charge", "consumption", "engine_power", "engine_torque",
-                        "location", "node", "charging", "distance_covered"])
+        
+        # Include timestamp in the header
+        writer.writerow(["timestamp", "car_id", "model", "current_speed", "battery_capacity", 
+                         "charge", "consumption", "engine_power", "engine_torque",
+                         "location", "node", "charging", "distance_covered"])
 
         # Iterate over the messages in the Kafka stream
         for msg in consumer:
             data = msg.value
             print(f"Received data from {topic_name}: {data}")
             
-            # Write the data to the CSV file
-            writer.writerow([data[key] for key in data])
+            # Capture the current timestamp when data is received
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            
+            # Write the timestamp followed by the other data fields to the CSV file
+            writer.writerow([timestamp] + [data[key] for key in data])
 
 if __name__ == "__main__":
     start_consumer('node2_data')
