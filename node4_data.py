9b4463bcd3b20e2c2905463fe4bd09a4fa254f69
Maitham Al-rubaye <maythem.altamar@gmail.com>
2023-08-22 02:31:50 +0200

    update nodes

--- node4_data.py
+++ node4_data.py
@@ -6,26 +6,31 @@ def start_consumer(topic_name):
     consumer = KafkaConsumer(
         topic_name,
         bootstrap_servers='localhost:9092',
-        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+        value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+        auto_offset_reset='earliest'  # To start reading from the beginning of the topic
     )
+
+    # Define the columns for the CSV
+    columns = [
+        "timestamp", "car_id", "model", "current_speed", "battery_capacity", 
+        "charge", "consumption", "engine_power", "engine_torque",
+        "location", "node", "charging", "distance_covered", "battery_life"
+    ]
 
     # Create a CSV file and write the headers
     with open(f'{topic_name}.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         
-        writer.writerow(["simulated_timestamp", "car_id", "model", "current_speed", "battery_capacity", 
-                         "charge", "consumption", "engine_power", "engine_torque",
-                         "location", "node", "charging", "distance_covered"])
+        # Write headers to CSV
+        writer.writerow(columns)
 
         # Iterate over the messages in the Kafka stream
         for msg in consumer:
             data = msg.value
             print(f"Received data from {topic_name}: {data}")
             
-            simulated_timestamp = data['timestamp']
-            
-            # Write the data to the CSV file
-            writer.writerow([simulated_timestamp] + [data[key] for key in data])
+            # Write the data to the CSV file using the order defined in columns
+            writer.writerow([data[col] for col in columns])
 
 if __name__ == "__main__":
     start_consumer('node4_data')
