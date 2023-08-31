eb94a3ae47c4114ec71b3469d812df731ca08912
Maitham Al-rubaye <maythem.altamar@gmail.com>
2023-08-31 21:08:34 +0200

    nodes scripts

--- nodes/node1_data.py
+++ nodes/node1_data.py
@@ -2,36 +2,55 @@ import csv
 from kafka import KafkaConsumer
 import json
 
-def start_consumer(topic_name):
-    consumer = KafkaConsumer(
-        topic_name,
-        bootstrap_servers='localhost:9092',
-        value_deserializer=lambda v: json.loads(v.decode('utf-8')),
-        auto_offset_reset='earliest'  # To start reading from the beginning of the topic
-    )
-
-    # Define the columns for the CSV
-    columns = [
-        "timestamp", "car_id", "model", "current_speed", "battery_capacity",
-        "charge", "consumption", "location", "node", "car_status",
-        "distance_covered", "battery_life", "distance_to_charging_point",
-        "weather", "traffic", "road_gradient", "emergency", "emergency_duration"
-    ]
-
-    # Create a CSV file and write the headers
-    with open(f'{topic_name}.csv', 'w', newline='') as file:
+def create_csv_writer(filename, columns):
+    try:
+        file = open(filename, 'w', newline='')
         writer = csv.writer(file)
-        
-        # Write headers to CSV
         writer.writerow(columns)
+        return file, writer
+    except Exception as e:
+        print(f"Error creating CSV writer: {e}")
+        raise
 
-        # Iterate over the messages in the Kafka stream
-        for msg in consumer:
-            data = msg.value
-            print(f"Received data from {topic_name}: {data}")
-            
-            # Write the data to the CSV file using the order defined in columns
-            writer.writerow([data[col] for col in columns])
+def write_data_to_csv(writer, data, columns):
+    try:
+        writer.writerow([data[col] for col in columns])
+    except Exception as e:
+        print(f"Error writing data to CSV: {e}")
+        raise
+
+def start_consumer(topic_name, csv_filename):
+    file = None
+    try:
+        consumer = KafkaConsumer(
+            topic_name,
+            bootstrap_servers='localhost:9092',
+            value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+            auto_offset_reset='earliest'
+        )
+
+        columns = [
+            "timestamp", "car_id", "model", "current_speed", "battery_capacity",
+            "charge", "consumption", "location", "node", "car_status",
+            "distance_covered", "battery_life", "distance_to_charging_point",
+            "weather", "traffic", "road_gradient", "emergency", "emergency_duration"
+        ]
+
+        file, writer = create_csv_writer(csv_filename, columns)
+
+        try:
+            for msg in consumer:
+                data = msg.value
+                print(f"Received data from {topic_name}: {data}")
+                write_data_to_csv(writer, data, columns)
+                file.flush()
+        except KeyboardInterrupt:
+            pass
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if file:
+            file.close()
 
 if __name__ == "__main__":
-    start_consumer('node1_data')
+    start_consumer('node1_data', 'node1_data.csv')
